5ddfd1937dbcc3d3eb06a42341185dbd99e10ea3
andfisher <and.fisher@hotmail.com>
2018-07-03 19:10:07 +0100

    Re-written the idle animation and tested it on the hardware

--- And_NeutrinoWandBarGraph.cpp
+++ And_NeutrinoWandBarGraph.cpp
@@ -1,15 +1,179 @@
 
 #include "Arduino.h"
+#include <Wire.h> // Include the I2C library (required)
+#include <SparkFunSX1509.h> // Include SX1509 library
 #include "And_NeutrinoWandBarGraph.h"
 
+/**
+ * @TODO
+ * Implement fallback for anyone not usin an SX1509
+ *
+And_NeutrinoWandBarGraph::And_NeutrinoWandBarGraph(int pins[], int pinCount) 
+{
+	_useSX1509 = false;
+	_pins = pins;
+	_setup();
+}
+*/
 And_NeutrinoWandBarGraph::And_NeutrinoWandBarGraph() 
 {
 }
 
-void And_NeutrinoWandBarGraph::setSpeed(int s)
+void And_NeutrinoWandBarGraph::init(SX1509 io, int pins[], int pinCount)
 {
+	_useSX1509 = true;
+	_io = io;
+	_pins = pins;
+	_pinCount = pinCount;
+	_setup();
 }
 
-void And_NeutrinoWandBarGraph::setPowerLevel(int s)
+void And_NeutrinoWandBarGraph::_setup()
 {
+	_begun = false;
+	_active = false;
+	_speed = SPEED_MINIMAL;
+	_currentIndex = 0;
+
+	for (int i = 0; i < _pinCount; i++) {
+		_pinMode(_pins[i], OUTPUT);
+		_digitalWrite(_pins[i], LOW);
+	}
+}
+
+void And_NeutrinoWandBarGraph::begin(long startTime)
+{
+	if (! _begun) {
+		_startTime = startTime;
+		_begun = true;
+	}
+}
+
+void And_NeutrinoWandBarGraph::_pinMode(int p, byte mode)
+{
+	if (_useSX1509) {
+		_io.pinMode(p, mode);
+	} else {
+		pinMode(p, mode);
+	}
+}
+
+int And_NeutrinoWandBarGraph::_digitalRead(int p)
+{
+	if (_useSX1509) {
+		return _io.digitalRead(p);
+	}
+	
+	return digitalRead(p);
+}
+
+
+void And_NeutrinoWandBarGraph::_digitalWrite(int p, int val)
+{
+	if (_useSX1509) {
+		_io.digitalWrite(p, val);
+	} else {
+		digitalWrite(p, val);
+	}
+}
+
+/**
+ * Suggested speeds:
+ * 10 - Extreme
+ * 20 - Seminal
+ * 50 - Nominal
+ * 100 - Minimal
+ */
+void And_NeutrinoWandBarGraph::setSpeed(int s)
+{
+	_speed = s;
+}
+
+void And_NeutrinoWandBarGraph::setPowerLevel(int p)
+{
+	switch(p) {
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		_power = p;
+	}
+}
+
+void And_NeutrinoWandBarGraph::idle(long currentTime)
+{
+	if (_active) {
+		_startTime = currentTime;
+		_active = false;
+		_currentIndex = 0;
+	}
+	
+	// No power? No bargraph animation
+	if (_power == 0) {
+		return;
+	}
+
+	// _power = 0, no bars lit
+	// _power = 1, bars 1-5 lit
+	// _power = 2, bars 1-10 lit
+	// _power = 3, bars 1-15 lit
+	int maxLitBars = (_power * 5);
+
+	// How many milliseconds have passed since the animation started?
+	unsigned long diff = currentTime - _startTime;
+	
+	// Based on the speed, how many animation frames have passed?
+	// (One "keyframe" equals one lit LED)
+	int frames = diff / _speed;
+	
+	// A local Index
+	int localIndex = frames % maxLitBars;
+	
+	int cycle = frames / maxLitBars;
+	bool asc = (cycle % 2 == 1); // only ascend bars on ODD cycles
+	
+	int inverseIndex = (_pinCount - localIndex) -1;
+	
+	/*
+	For debugging long type limits affecting the animation
+	Serial.print(diff);
+	Serial.print(" - ");
+	Serial.println(frames);
+	*/
+	
+	for (int i = 0; i < _pinCount; i++) {
+	
+		if (asc) {
+	
+			if (i < localIndex) {
+				_digitalWrite(i, HIGH);
+			} else {
+				_digitalWrite(i, LOW);
+			}
+		
+		} else {
+		
+			if (i <= inverseIndex) {
+				_digitalWrite(i, HIGH);
+			} else {
+				_digitalWrite(i, LOW);
+			}
+		
+		}
+	}
+}
+
+void And_NeutrinoWandBarGraph::activate(long currentTime)
+{
+	if (! _active) {
+		_startTime = currentTime;
+		_active = true;
+	}
+	
+	/**
+	 * @TODO
+	 * Implement the "activated" bargraph animation
+	 *
+	 */
+	
 }
--- And_NeutrinoWandBarGraph.h
+++ And_NeutrinoWandBarGraph.h
@@ -3,15 +3,38 @@
 #define And_NeutrinoWandBarGraph_h
 
 #include "Arduino.h"
+#include "SparkFunSX1509.h"
 
 class And_NeutrinoWandBarGraph {
 	public:
+		//And_NeutrinoWandBarGraph(int pins[], int pinCount);
 		And_NeutrinoWandBarGraph();
+		void init(SX1509 io, int pins[], int pinCount);
 		void setSpeed(int s);
 		void setPowerLevel(int p);
+		void begin(long startTime);
+		void idle(long currentTime);
+		void activate(long currentTime);
+		
+		const static int SPEED_MINIMAL = 100;
+		const static int SPEED_NOMINAL = 50;
+		const static int SPEED_SEMIMAL = 20;
+		const static int SPEED_EXTREME = 10;
 	private:
+		SX1509 _io;
+		bool _useSX1509;
+		void _setup();
+		void _pinMode(int p, byte mode);
+		int _digitalRead(int p);
+		void _digitalWrite(int p, int val);
+		int* _pins;
 		int _power;
 		int _speed;
+		bool _begun;
+		bool _active;
+		unsigned long _startTime;
+		int _pinCount;
+		int _currentIndex;
 };
 
 #endif
